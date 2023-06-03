9ac7441424a6f307c9d2a35c182ff1f7a3c74c9c
Vava <57761970+vava72@users.noreply.github.com>
2023-06-03 13:54:24 +0200

    add options

---
+++ resources/views/admin/options/form.blade.php
@@ -0,0 +1,25 @@
+@extends('admin.properties.admin')
+
+@section('title', $option->exists ? "Editer une option" : "Créer une option")
+
+@section('content')
+
+    <h1>@yield('title')</h1>
+
+    <form action="{{ route($option->exists ? 'admin.option.update' : 'admin.option.store', ['option' => $option]) }}" method="POST">
+        @csrf
+        @method($option->exists ? 'put' : 'post')
+
+        @include('shared.input', ['name' => 'name', 'label' => 'Nom', 'value' => $option->name])
+
+
+        <button class="form-submit-btn">
+            @if($option->exists)
+                Modifier
+            @else
+                Créer
+            @endif
+        </button>
+    </form>
+
+@endsection
