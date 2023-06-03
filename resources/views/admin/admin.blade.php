52280683b6819a0d9cfeef62833467f107f860c8
Vava <57761970+vava72@users.noreply.github.com>
2023-06-03 18:10:38 +0200

    style edit correction

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -5,13 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') | Administration</title>
-    <link rel="stylesheet" href="../../css/style.css" >
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" >
 </head>
 <body>
 
     <nav class="navbar">
         <div class="nav-container">
-            <a class="navbar-home" href="/admin/property">Agence</a>
+            <a class="navbar-home" href="/admin/property"><img src="https://placehold.co/200x50"></a>
+            
             @php
             $route = request()->route()->getName();
             @endphp
