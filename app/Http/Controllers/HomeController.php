636146882a4c4a109226c3fc7c9e9a547dbbbbe2
babawebelise <bascopvalentin7@gmail.com>
2023-06-09 16:31:45 +0200

    aaa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Property;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,7 +11,8 @@ class HomeController extends Controller
     public function index()
     {
         // Retrieve the 6 most recent properties, ordered by their creation date
-        $properties = Property::orderBy('created_at', 'desc')->limit(6)->get();
+        // Si la méthode s'appelle "scopeAvailable()", on met juste "available()"
+        $properties = Property::with('pictures')->available()->recent()->limit(6)->get();
 
         // Return the 'home' view and pass the properties to the view
         return view('home', ['properties' => $properties]);
--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Property extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    
 
     protected $fillable = [
         'title',
@@ -25,7 +30,7 @@ class Property extends Model
         'postal_code',
         'sold',
     ];
-
+    
     public function options(): BelongsToMany
     {
         return $this->belongsToMany(Option::class);
@@ -64,5 +69,15 @@ class Property extends Model
 
     public function getPicture(): ?Picture{
         return $this->pictures[0] ?? null;
+    }
+
+    public function scopeAvailable(Builder $builder, bool $available = true): Builder {
+        // return $builder->where('sold', false);
+        // Equivalent pour le boolean : 
+        return $builder->where('sold', !$available);
+    }
+
+    public function scopeRecent(Builder $builder): Builder {
+        return $builder->orderBy('created_at', 'desc');
     }
 }
