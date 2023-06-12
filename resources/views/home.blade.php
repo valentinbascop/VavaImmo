1748ddf3477fcb2f7c483805a23e7cf9a1bf684b
babawebelise <bascopvalentin7@gmail.com>
2023-06-12 16:03:15 +0200

    taff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,14 @@
     <div class="container">
 
         <h2 class="listing-title">Biens proposés les plus récents</h2>
-        <div class="listing-properties">
-            @foreach($properties as $property)
-            <div class="list-element">
-                @include('property.card')
+        <div class="listing-container">
+            <div class="listing-properties">
+                @foreach($properties as $property)
+                <div class="list-element">
+                    @include('property.card')
+                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
 
